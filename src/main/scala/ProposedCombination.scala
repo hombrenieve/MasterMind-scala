12d11ef190164f01b8b79556acd91b05c70860c4
Pablo Rebaque Diaz <hombrenieve@gmail.com>
2020-02-07 22:59:20 +0100

    Converted to tail recursion

--- src/main/scala/ProposedCombination.scala
+++ src/main/scala/ProposedCombination.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 class ProposedCombination(val combination: List[Color.Color]) {
 
   private object Success extends Enumeration {
@@ -24,18 +26,22 @@ class ProposedCombination(val combination: List[Color.Color]) {
 
   private def markWhites(color: Color.Color, secret: List[Color.Color], result: List[Success.Success]): List[Success.Success] = {
     require(secret.size == result.size)
-    (secret, result) match {
-      case (hs :: Nil, Success.EMPTY :: Nil) if hs == color => List(Success.WHITE)
-      case (_ :: Nil, hr :: Nil) => List(hr)
-      case (hs :: _, Success.EMPTY :: tr) if hs == color => Success.WHITE :: tr
-      case (_ :: ts, hr :: tr) => hr :: markWhites(color, ts, tr)
+    @tailrec
+    def markWhitesTail(color: Color.Color, secret: List[Color.Color], result: List[Success.Success], acc: List[Success.Success]): List[Success.Success] = {
+      (secret, result) match {
+        case (hs :: Nil, Success.EMPTY :: Nil) if hs == color => acc ::: List(Success.WHITE)
+        case (_ :: Nil, hr :: Nil) => acc ::: List(hr)
+        case (hs :: _, Success.EMPTY :: tr) if hs == color => acc ::: Success.WHITE :: tr
+        case (_ :: ts, hr :: tr) => markWhitesTail(color, ts, tr, acc ::: List(hr))
+      }
     }
+    markWhitesTail(color, secret, result, Nil)
   }
 
   private def calculateWhitesResult(combination: List[Color.Color], secret: List[Color.Color], result: List[Success.Success]): List[Success.Success] = {
     require(combination.size == result.size)
-    val combinationsLeft = combination.zip(result).filter(item => item._2 == Success.EMPTY).map(item => item._1)
-    combinationsLeft.foldLeft(result)((prevResult, color) => markWhites(color, secret, prevResult))
+    val combinationsPending = combination.zip(result).filter(item => item._2 == Success.EMPTY).map(item => item._1)
+    combinationsPending.foldLeft(result)((prevResult, color) => markWhites(color, secret, prevResult))
   }
 
 
